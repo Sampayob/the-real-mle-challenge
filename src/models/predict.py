df94b585d593aaf6192a564adf84e68a2c235123
Sampayob <sampayo.b@gmail.com>
2023-02-14 15:31:41 +0100

    Added ValueError check for inputs

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 from config.config import FILEPATH_MODEL
 from src.utils.utils import encode_categorical_features
@@ -11,7 +12,9 @@ def predict_pipeline(inputs: dict, train_args: dict) -> int:
     # Select features
     inputs = {k:v for k, v in inputs.items() if k in train_args['features_selected']}
     # Predict
-    inputs = [[v for k, v in inputs.items()]] 
+    inputs = np.array([[v for k, v in inputs.items()]])
+    if inputs.ndim != 2:
+        ValueError("'inputs' array does not have 2 dimensions.")
     model = pickle.load(open(str(FILEPATH_MODEL), 'rb'))
     y_pred = model.predict(inputs)
     return y_pred
