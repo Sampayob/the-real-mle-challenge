d2bc4de0487099e24a4cb3e1ed98cde6bf3ae4f0
Sampayob <sampayo.b@gmail.com>
2023-02-14 15:32:12 +0100

    Added ValueError checks for setup and train

--- src/models/train.py
+++ src/models/train.py
@@ -10,9 +10,13 @@ def setup(df: DataFrame, args: dict) -> tuple:
     cat_maps = args['categorical_encoding']
     df["neighbourhood"] = df["neighbourhood"].map(cat_maps["MAP_NEIGHB"])
     df["room_type"] = df["room_type"].map(cat_maps["MAP_ROOM_TYPE"])
+    if df["neighbourhood"].isnull().any() or df["neighbourhood"].isnull().any():
+        raise ValueError("Some mapping key: value are wrong because 'NaN' values are present.")
     # train-test split
     X = df[args['features_selected']].values
     y = df['category'].values
+    if X.ndim != 2:
+        raise TypeError("args['features_selected'] is not a feature list.")
     X_train, X_test, y_train, y_test = train_test_split(X, y, **args['train_test_split_args'])
     return X_train, X_test, y_train, y_test
 
@@ -21,6 +25,10 @@ def train(df: DataFrame, args: dict) -> dict:
     """Train model."""
     # Setup
     X_train, X_test, y_train, y_test = setup(df, args)
+    if all(x.ndim != 2 for x in [X_train, X_test]):
+        ValueError("X_train and/or X_test don't have 2 dimensions.")
+    if all(x.ndim != 1 for x in [y_train, y_test]):
+        ValueError("y_train and/or y_test don't have 1 dimensions.")
     # Model
     clf = RandomForestClassifier(**args['model_args'])
     # Training
@@ -31,5 +39,4 @@ def train(df: DataFrame, args: dict) -> dict:
             "X_train": X_train,
             "y_train": y_train,
             "X_test": X_test,
-            "y_test": y_test
-            }
+            "y_test": y_test}
